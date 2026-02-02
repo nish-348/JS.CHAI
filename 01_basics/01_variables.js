c1ee660643d7403dc8a4498c6de9a7a41f124fa0
nish-348 <2024ugec034@nitjsr.ac.in>
2026-02-02 03:29:40 +0000

    add initial variable declarations and logging to 01_variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144553
+let accountEmail  = "nishthathis@gmail.com"
+var accountpassword ="12345"
+accountcity = "jaipur"
+
+//accouuntId=2//cant give assignment to constant variable
+
+accountEmail="hello@false.com "
+accountpassword="67890"
+accountcity="delhi"
+console . log(accountId);
+
+console.table( accountEmail, accountpassword, accountcity);
+//table is to make a tabular structure in console using comma
