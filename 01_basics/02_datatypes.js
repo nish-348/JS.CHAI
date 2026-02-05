16533671b64cad9fc9a5f5bba3e83322331734e2
nish-348 <2024ugec034@nitjsr.ac.in>
2026-02-05 03:52:07 +0000

    feat: add initial data types examples and explanations in 02_datatypes.js

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"// sabkuch ko newer version of javasscrit me shiftho jaayega
+
+//alert("hello world")we are using nodejs....not browser...alert works in browser
+
+//code readable honaa chayiye....readability should be high...acche se likhna h,
+
+let name="nishtha"    //string
+let age=20            //numbers
+let isloggedin=true   //boolean
+
+//number ka range hota hai...-2^53 to 2^53
+//bigint is used for very large numbers
+//string => ""
+//boolean => true/false
+//null => empty value
+//undefined => value is not assigned
+//symbol => unique identifier
+
+//object later batayegaa
+console.log(typeof "nishtha")
+
+//null ka type object hota hai...ye ek bug hai javascript ka...null is not an object but it returns object when we check its type
+
+//undefined ka type bhi undefined hota hai...ye sahi hai
+
+
