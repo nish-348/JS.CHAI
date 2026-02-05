30f8a7e0afc52059fc6d7abe58a295fce2bc129c
nish-348 <2024ugec034@nitjsr.ac.in>
2026-02-05 15:17:37 +0000

    refactor: comment out console.log statements for clarity in conversion operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -1,14 +1,14 @@
 let score ="chutiya"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 // both the live have same context...brackets dont change anything
 let valueInNumber=Number(score)
 
 //score ka jo bhi type hoga usko ab number me convert kar dega
-console.log(typeof valueInNumber);
+//console.log(typeof valueInNumber);
 //abhi tak number h
-console.log(valueInNumber);
+//console.log(valueInNumber);
 //NaN means not a number...it is a special type of number which shows that the value is not valid number
 //beacause "34avk" is not a valid number...actual value nahi h
 
@@ -22,7 +22,7 @@ console.log(valueInNumber);
 
 let rat="0"
 let booleanrat=Boolean(rat)
-console.log(booleanrat);
+//console.log(booleanrat);
 
 //"1"=>true
 //"0"=>true
@@ -33,3 +33,17 @@ console.log(booleanrat);
 //undefined=>false
 //null=>false
 //string "true" is converted to boolean true...because it is a non empty string...if it was an empty string then it would have been converted to false
+
+
+//******************************************operations**********************************************************************************************
+
+
+let a=34
+let negvalue=-34
+//console.log(negvalue);
+
+//console.log(2+"2")//string concatenation...number 2 is converted to string "2" and then concatenated with "2" to give "22"
+//console.log("2"+2)//string concatenation...number 2 is converted to string "2" and then concatenated with "2" to give "22"
+//console.log("5"-2)//subtraction operation...string "5" is converted to number 5 and then 2 is subtracted from it to give 3
+//console.log("5"*2)//multiplication operation...string "5" is converted to number 5 and then multiplied by 2 to give 10
+//console.log("5"/2)//division operation...string "5" is converted to number 5 and then divided by 2 to give 2.5
