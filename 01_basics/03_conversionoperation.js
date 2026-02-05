f36d8d7e0fdc02ef90136bb13f0767930b9f6ffb
nish-348 <2024ugec034@nitjsr.ac.in>
2026-02-05 07:39:24 +0000

    feat: add conversion operations examples and explanations in 03_conversionoperation.js

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,35 @@
+let score ="chutiya"
+
+console.log(typeof score);
+console.log(typeof (score));
+// both the live have same context...brackets dont change anything
+let valueInNumber=Number(score)
+
+//score ka jo bhi type hoga usko ab number me convert kar dega
+console.log(typeof valueInNumber);
+//abhi tak number h
+console.log(valueInNumber);
+//NaN means not a number...it is a special type of number which shows that the value is not valid number
+//beacause "34avk" is not a valid number...actual value nahi h
+
+
+//"34"=> 34
+//"34abc"=> NaN
+//"true"=> 1
+//"false"=>0
+//undefined=>NaN
+//null=>0
+
+let rat="0"
+let booleanrat=Boolean(rat)
+console.log(booleanrat);
+
+//"1"=>true
+//"0"=>true
+//""=>false
+//"nishtha"=>true
+//0=>false
+//1=>true
+//undefined=>false
+//null=>false
+//string "true" is converted to boolean true...because it is a non empty string...if it was an empty string then it would have been converted to false
