dd0414bcd8c3875fdd79349ee649021843b4101c
nish-348 <2024ugec034@nitjsr.ac.in>
2026-02-05 03:14:31 +0000

    update accountEmail value and include accountstate in console.table

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,13 +2,14 @@ const accountId =144553
 let accountEmail  = "nishthathis@gmail.com"
 var accountpassword ="12345"
 accountcity = "jaipur"
+let accountstate;
 
 //accouuntId=2//cant give assignment to constant variable
 
-accountEmail="hc@hc.com "
+accountEmail="dgfdhv@hc.com "
 accountpassword="67890"
 accountcity="delhi"
 console . log(accountId);
 
-console.table([accountId,accountEmail, accountpassword, accountcity]);
+console.table([accountEmail, accountpassword, accountcity,accountId,accountstate]);
 //table is to make a tabular structure in console using comma
