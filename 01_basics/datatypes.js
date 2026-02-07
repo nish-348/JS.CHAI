aba1435e3f76a368679a31f58c8044f9351c64a1
nish-348 <2024ugec034@nitjsr.ac.in>
2026-02-07 06:01:54 +0000

    feat: add initial datatypes documentation in datatypes.js

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,10 @@
+//primitive
+
+//types:string,number,boolean,null,undefined,symbol,BigInt
+
+//refference(non primitive)
+
+//types:object,array,function
+
+//primitive types ke andar jo value store hoti hai wo directly variable ke andar store hoti hai
+//refference types ke andar jo value store hoti hai wo memory me kisi alag jagah store hoti hai, aur variable ke andar us jagah ka address store hota hai
