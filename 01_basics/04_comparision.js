efeeceaf7b8dcc3c4161a14b84dd0975f5a7387d
nish-348 <2024ugec034@nitjsr.ac.in>
2026-02-07 05:17:45 +0000

    feat: add comparison operations examples and explanations in 04_comparision.js

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,22 @@
+//console.log(2>1)
+//console.log(2<1)
+//console.log(2>=1)
+//console.log(2<=1)
+//console.log(2==1)
+//console.log(2!=1)
+
+//yeh comparision easy h....but jab 2 datatype ko compare karenge tab issue hoga
+
+//console.log(null>0)
+//console.log(null==0)
+//console.log(null>=0)
+
+//operators differently work karta hai jab hum different datatypes ko compare karte hain.
+// isiliyewe last me null ko 0 maan ke kaam kiya
+
+//===    strict checking operator
+console'log("2"===2) //false
+console.log("2"==2) //true
+
+//=== operator check karta hai ki dono value same hain ya nahi, aur dono ka datatype same hai ya nahi
+//== operator check karta hai ki dono value same hain ya nahi, lekin datatype ko ignore karta hai
