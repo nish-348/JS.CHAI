54f468688dfab579e21ffe62d87b1b8c1362444c
nish-348 <2024ugec034@nitjsr.ac.in>
2026-02-11 08:52:30 +0000

    feat: add initial string handling examples in 05_strings.js

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,42 @@
+//singhle ya double quotes dono see string declare h jaayega
+
+//const name="John";
+//const repo=56
+//console.log(name + repo+ "ji")
+
+//very outdated 
+
+//we will use back ticks to declare string in modern js`````
+
+//console.log(`my name is ${name} and i have ${repo} repo`    )
+
+//one more way to declare a String
+const game= new String('Call of Duty');
+//console.log(game[0])// yeh indexing h string ka
+//console.log(game[3])
+
+//object access  karne ke liyesyntax h
+
+//console.log(game.__proto__);
+
+//example
+//console.log(game.length)
+//console.log(game.toUpperCase())
+
+//console.log(game.toLowerCase())
+//console.log(game.charAt(9))
+//console.log(game.indexOf('o'))
+
+//to divide in substring
+//const newString=game.substring(1,5)
+//console.log(newString)
+
+//slice,trim,replace
+//console.log(game.slice(1,5))
+//const newstring="    hjgjg    "    //yeh trim method se string ke starting aur ending ke spaces ko remove kar deta h
+//console.log(newstring.trim())
+
+
+//const url="tickij//nhh@jgh"
+//replace use karenge to replace something in string
+//console.log(url.replace('tickij','https'))
