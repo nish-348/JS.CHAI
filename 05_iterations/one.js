5cceca2955905693a9e5ea1b3454540d6805171d
nish-348 <2024ugec034@nitjsr.ac.in>
2026-04-06 14:47:48 +0000

    feat: add for loop example demonstrating iteration and conditional logging in one.js

---
+++ 05_iterations/one.js
@@ -0,0 +1,17 @@
+//for loop
+//for (let index = 0; index < array.length; index++) {
+  //  const element = array[index];
+    
+//}
+
+    
+for(let i = 0; i <=10; i++) {
+    const element = i;
+    if(i === 5){
+        console.log("i is 5");
+
+    }
+    console.log(element);
+
+    
+}
