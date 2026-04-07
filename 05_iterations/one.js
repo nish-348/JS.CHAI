c2d8d6676960de4a06f6991e5bfb3999d6cf45ce
nish-348 <2024ugec034@nitjsr.ac.in>
2026-04-07 12:46:17 +0000

    feat: update for loop examples and add explanations for break and continue statements in one.js

--- 05_iterations/one.js
+++ 05_iterations/one.js
@@ -5,13 +5,46 @@
 //}
 
     
-for(let i = 0; i <=10; i++) {
-    const element = i;
-    if(i === 5){
-        console.log("i is 5");
+// for(let i = 0; i <=10; i++) {
+//     const element = i;
+//     if(i === 5){
+//         console.log("i is 5");
 
-    }
-    console.log(element);
+//     }
+//     console.log(element);
 
     
+// }
+// for(let i = 0; i <=10; i++) {
+// console.log(`outer loop ${i}`);
+//     for(let j = 0; j <=10; j++) {
+//         console.log(`inner loop value ${j} and inner loop ${i}`);
+//         console.log(i +'*'+ j +'=' +i*j); 
+//     }
+// }
+let myarray=["apple", "banana", "orange", "grape", "kiwi"];
+for(let i = 0; i < myarray.length; i++) {
+    const element = myarray[i];
+    console.log(element);
+}
+
+//break and continue
+
+//break is used to exit the loop when a certain condition is met
+//continue is used to skip the current iteration and move to the next iteration when a certain condition is met
+
+for(let i = 0; i <=10; i++) {
+    if(i === 5){
+        console.log("i is 5");
+        break;
+    }
+    console.log(i);
+}
+
+for(let i = 0; i <=10; i++) {
+    if(i === 5){
+        console.log("i is 5");
+        continue;
+    }
+    console.log(i);
 }
